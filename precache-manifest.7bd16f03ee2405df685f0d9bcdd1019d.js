8efadc6237b52f95f8e35b12188d59338789e496
BK1917caoJianRong <1850125484@qq.com>
2020-02-18 18:14:19 +0800

    阿里首页

---
+++ precache-manifest.7bd16f03ee2405df685f0d9bcdd1019d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa92ae077d3e9091010a8625ca11b42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86c70737f002135708e0",
+    "url": "./static/css/main.158c5433.chunk.css"
+  },
+  {
+    "revision": "1f91ddcbac17faa68b97",
+    "url": "./static/js/2.493f1358.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.493f1358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c70737f002135708e0",
+    "url": "./static/js/main.1b09f619.chunk.js"
+  },
+  {
+    "revision": "4511b495726514be15c4",
+    "url": "./static/js/runtime-main.7d9b66b1.js"
+  }
+]);
